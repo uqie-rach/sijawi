8f5fb7d05a88aa96d3d96b20261cb9cad8e3132c
[dyad] <achmadfurqonrachmadie@gmail.com>
2026-07-02 16:26:05 +0700

    [dyad] Updating bcrypt utility to support password hashing - wrote 1 file(s)

--- src/lib/bcrypt.ts
+++ src/lib/bcrypt.ts
@@ -1,7 +1,10 @@
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
 export const bcrypt = {
   async compare(plainText: string, hashedText: string): Promise<boolean> {
     return await compare(plainText, hashedText);
+  },
+  async hash(plainText: string): Promise<string> {
+    return await hash(plainText, 10);
   },
 };
